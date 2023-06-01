c8bffd7269f56dadd292d3f965ce7e90932aa145
Alejandro Noel Hernández <alejandro.noel.hernandez@gmail.com>
2023-06-01 08:24:44 -0600

    Big O

---
+++ 02-Big_0/9. O(1).py
@@ -0,0 +1,6 @@
+def add_items(n):
+    return n + n + n
+
+# If you have an arithmetical sum is one operation
+# O(1) is also call constant time, meaning that as in increases the number of operations as sum it will be constant
+print(add_items(10))
